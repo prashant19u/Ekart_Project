5698f19b784667565c86b998385c0b38cc78ab05
Prashant Kumar <prashant19u@gmail.com>
2021-11-01 12:42:16 +0530

    First Commit

---
+++ src/main/java/com/prashant/ekart/EkartServer_Spring/EkartServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.ekart.EkartServer_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartServerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkartServerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prashant/ekart/EkartServer_Spring/EkartServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.ekart.EkartServer_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkartServerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
